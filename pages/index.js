8b6ca61d915785c0595191c007944ed62f71b5c9
civsavteam <info@civsav.com>
2025-08-01 19:23:03 -0700

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import Link from 'next/link';
 import Head from 'next/head';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Image from 'next/image';
+import Link from 'next/link';
 
 /**
  * Home page
